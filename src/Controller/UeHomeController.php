bfa62760d964e8354658384c4e2da0a602d8c114
MouradK2003 <mourad.ketata@utbm.fr>
2025-04-26 10:49:31 +0200

    affichage nom correct dans ue_home

--- src/Controller/UeHomeController.php
+++ src/Controller/UeHomeController.php
@@ -6,13 +6,15 @@ use App\Repository\UERepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\SecurityBundle\Security;
 
 final class UeHomeController extends AbstractController
 {
     #[Route('/ue/home_page', name: 'ue_home')]
-    public function index(UERepository $ueRepository): Response
+    public function index(UERepository $ueRepository, Security $security): Response
     {
         $ues = $ueRepository->findAll();
+        $user = $security->getUser(); // récupère l'utilisateur connecté
 
         return $this->render('ue_home/index.html.twig', [
             'styles' => ['ue_home_style'],
@@ -20,6 +22,8 @@ final class UeHomeController extends AbstractController
             'footer' => 'PageParts/footer.html.twig',
             'currentPage' => 'ue_home',
             'ues' => $ues,
+            'user' => $user,
         ]);
     }
 }
+
