59677069bf937761ba841ab9322ec9c1ea6f21d6
MouradK2003 <mourad.ketata@utbm.fr>
2025-04-26 19:02:43 +0200

    ajout, modif, suppression post en bdd et en affichage

--- src/Controller/UeContentController.php
+++ src/Controller/UeContentController.php
@@ -2,26 +2,35 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\UE;
+use App\Repository\PostRepository;
+use App\Repository\UERepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\UERepository;
-use App\Entity\Post;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Post;
 
 final class UeContentController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/ue/content/{id}', name: 'ue_content')]
-    public function index(int $id, UERepository $ueRepository, EntityManagerInterface $entityManager): Response
+    public function index(int $id, UERepository $ueRepository, PostRepository $postRepository): Response
     {
         $ue = $ueRepository->find($id);
-
-        $posts = $entityManager->getRepository(Post::class)->findBy(['id_ue' => $id], ['post_date' => 'DESC']);
 
         if (!$ue) {
             throw $this->createNotFoundException('UE non trouvée.');
         }
+
+        $posts = $postRepository->findBy(['id_ue' => $ue]);
 
         return $this->render('ue_content/index.html.twig', [
             'styles' => ['UE_page_style', 'UE_prof'],
@@ -30,8 +39,73 @@ final class UeContentController extends AbstractController
             'footer' => 'PageParts/footer.html.twig',
             'currentPage' => 'ue_content',
             'ue' => $ue,
-            'postlist' => $posts,
+            'posts' => $posts,
         ]);
     }
+
+    #[Route('/post/add/{id}', name: 'post_add')]
+    public function addPost(Request $request, EntityManagerInterface $em, UE $ue): Response
+    {
+        $post = new Post();
+        $post->setIdUe($ue);
+
+        $title = $request->request->get('title');
+        $content = $request->request->get('content');
+        $file = $request->files->get('file');
+
+        $post->setTitle($title);
+        $post->setContent($content);
+
+        if ($file) {
+            $filename = uniqid().'.'.$file->guessExtension();
+            $file->move($this->getParameter('kernel.project_dir') . '/public/uploads', $filename);
+            $post->setFile($filename);
+        }
+        $post->setIdUser($this->getUser());
+
+        $post->setPostDate(new \DateTime());
+
+        $em->persist($post);
+        $em->flush();
+
+        return $this->redirectToRoute('ue_content', ['id' => $ue->getId()]);
+    }
+
+    #[Route('/post/{id}/delete', name: 'post_delete')]
+    public function delete(int $id): Response
+    {
+        // Utiliser l'entityManager injecté pour supprimer le post
+        $post = $this->entityManager->getRepository(Post::class)->find($id);
+
+        if ($post) {
+            $this->entityManager->remove($post);
+            $this->entityManager->flush();
+        }
+
+        return $this->redirectToRoute('ue_content', ['id' => $post->getIdUe()->getId()]);
+    }
+
+    #[Route('/post/{id}/update', name: 'post_update', methods: ['POST'])]
+    public function update(Request $request, PostRepository $postRepository, EntityManagerInterface $entityManager, $id): Response
+    {
+        $post = $postRepository->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('Post not found');
+        }
+
+        $title = $request->request->get('title');
+        $content = $request->request->get('content');
+
+        $post->setTitle($title);
+        $post->setContent($content);
+
+        $entityManager->flush();
+
+        // après update on peut revenir sur la page UE
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+
 
 }
