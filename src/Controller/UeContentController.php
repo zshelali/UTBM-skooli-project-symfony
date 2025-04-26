d911a0343a59c066ef17c40c35c54205eab48dbd
Titouan <titouan.escande@utbm.fr>
2025-04-26 08:53:10 +0200

    Modification UeContent

--- src/Controller/UeContentController.php
+++ src/Controller/UeContentController.php
@@ -2,21 +2,31 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Post;
+use App\Entity\UE;
 
 final class UeContentController extends AbstractController
 {
-    #[Route('/ue/content', name: 'ue_content')]
-    public function index(): Response
+    #[Route('/ue/content/{CodeUE}', name: 'ue_content')]
+    public function index(EntityManagerInterface $entityManager, string $CodeUE): Response
     {
+        $ue = $entityManager->getRepository(UE::class)->find($CodeUE);
+        $posts = $entityManager->getRepository(Post::class)->find($CodeUE);
+        ;
+
+
         return $this->render('ue_content/index.html.twig', [
             'styles' => ['UE_page_style', 'UE_prof'],
             'scripts' => ['AddPost_script'],
             'header' => 'PageParts/header.html.twig',
             'footer' => 'PageParts/footer.html.twig',
             'currentPage' => 'ue_content',
+            'postlist' => $posts,
+            'ue' => $ue,
         ]);
     }
 }
